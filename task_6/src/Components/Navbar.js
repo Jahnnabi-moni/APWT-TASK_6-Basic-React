91af490fba3f90ec3492dce51e6fca5e63d93644
Jahnnabi-moni <jahnnabi.moni54@gmail.com>
2021-12-04 21:45:15 +0600

    Committing task_6

---
+++ task_6/src/Components/Navbar.js
@@ -0,0 +1,14 @@
+import react from 'react';
+
+function Header()
+{
+    return (
+        <div>
+        <a href="/">Home </a>
+        <a href="/Contact"> Contact</a>
+        <a href="/Student"> Student</a>
+        </div>
+    )
+}
+
+export default Header;
